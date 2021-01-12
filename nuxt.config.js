1875dbb3fd1d946939ab8aadf7a037e69c86cb8f
lego3708 <l3gomindstorms@gmail.com>
2021-01-12 19:09:46 +0000

    Change router base

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,7 +3,7 @@ export default {
     target: 'static',
 
     router: {
-        base: '/website/'
+        base: '/'
     },
 
     // Global page headers (https://go.nuxtjs.dev/config-head)
@@ -15,7 +15,7 @@ export default {
             {hid: 'description', name: 'description', content: ''}
         ],
         link: [
-            {rel: 'icon', type: 'image/x-icon', href: '/website/favicon.ico'},
+            {rel: 'icon', type: 'image/x-icon', href: '/favicon.ico'},
             {rel: 'stylesheet', href: 'https://cdn.jsdelivr.net/npm/@mdi/font@5.8.55/css/materialdesignicons.min.css'}
         ]
     },
