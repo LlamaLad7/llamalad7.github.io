ca6ac9ae08ac85ecf704f7220d2d2cb9aa4759ab
LlamaLad7 <l3gomindstorms@gmail.com>
2024-02-04 19:30:08 +0000

    New: Add discord redirect.

---
+++ pages/discord.vue
@@ -0,0 +1,17 @@
+<template />
+
+<script>
+export default {
+    name: "discord",
+    head() {
+        return {
+            meta: [
+                {
+                    "http-equiv": "refresh",
+                    content: "0;url=https://discord.gg/VUkAeRYqdH"
+                }
+            ]
+        }
+    }
+}
+</script>
